23b1fe395aba6771177f76e4b58f84cbe4e2e001
Deniz Aydemir <d.aydemir@ug.bilkent.edu.tr>
2024-10-09 06:11:09 +0300

    added more proper shutdowning to server

--- server/server.go
+++ server/server.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"context"
 	"net"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/denizydmr07/zapwrapper/pkg/zapwrapper"
 	"go.uber.org/zap"
@@ -18,25 +23,51 @@ func main() {
 
 	defer logger.Sync() // Flush any buffered log entries
 
-	// Start the server
-	func() {
-		ln, err := net.Listen("tcp", ":8080")
-		logger.Info("Server started")
-		if err != nil {
-			logger.Error("Error in Listen", zap.Error(err))
-			return
-		}
-		defer ln.Close()
+	// Channel to listen SIGINT and SIGTERM
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, syscall.SIGINT, syscall.SIGTERM)
 
+	// Context to cancel the server
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	// Listen on port 8080
+	ln, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		logger.Error("Error in Listen", zap.Error(err))
+		return
+	}
+	defer ln.Close()
+	logger.Info("Server started")
+
+	// Start the server
+	go func() {
 		for {
 			conn, err := ln.Accept()
 			if err != nil {
-				logger.Error("Error in Accept", zap.Error(err))
-				return
+				select {
+				case <-ctx.Done():
+					return
+				default:
+					logger.Error("Error in Accept", zap.Error(err))
+					continue
+				}
 			}
 
 			logger.Info("Client connected", zap.String("address", conn.RemoteAddr().String()))
 			go stub.HandleConnection(conn)
 		}
 	}()
+
+	<-stop
+
+	// Stop the server
+	cancel()
+
+	// waiting 1 second
+	<-time.After(1 * time.Second)
+
+	// Close the listener
+	ln.Close()
+	logger.Info("Server stopped")
 }
